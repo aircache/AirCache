2bd846a497a93133f8bc22722fe25eff7016a569
Mehdi Roshan Fekr <mehdi.roshanfekr@qcentic.com>
2021-12-20 19:52:18 +0100

    :sparkles::bug::hammer: Redis caching supported added and fixed from core

--- utils/cache/__init__.py
+++ utils/cache/__init__.py
@@ -1,12 +1,17 @@
 import os
 
 from utils.cache.interface import ICacheOption
+from utils.cache.redis import RedisCaching
 from .memory import MemoryCaching
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config_env = os.environ.get('CACHE_OPTION', 'memory')
-
 
 def cache_option() -> ICacheOption:
     if(config_env == "memory"):
         return MemoryCaching
+    elif(config_env == "redis"):
+        return RedisCaching
