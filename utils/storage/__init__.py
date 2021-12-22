15fd1ebce2e8ca73774d516147a846bc672c97be
Mehdi Roshan Fekr <mehdi.roshanfekr@qcentic.com>
2021-12-22 12:12:56 +0100

    :sparkles: env base config storage added

--- utils/storage/__init__.py
+++ utils/storage/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from utils.storage.interface import IStorageOption
 from .file import FileStorage
+from .env_base64 import EnvStorage
 
 config_env = os.environ.get('STORAGE_OPTION', 'file')
 
@@ -9,3 +10,5 @@ config_env = os.environ.get('STORAGE_OPTION', 'file')
 def storage_option() -> IStorageOption:
     if(config_env == "file"):
         return FileStorage
+    elif(config_env == "env"):
+        return EnvStorage
